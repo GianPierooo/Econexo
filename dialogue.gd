6b0059258cc2adf2926effc39b280f1bd8fb2269
Gian Piero Cano <gianpierodaniel@gmail.com>
2026-04-29 17:34:04 -0500

    Prototipo dialogo facil
    Basicamente esta el dialogo.

--- dialogue.gd
+++ dialogue.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-var balloon_scene = preload("res://dialogues/balloon.tscn")
-
-func show_dialogue():
-	var balloon = balloon_scene.instantiate()
-	get_tree().current_scene.add_child(balloon)
-
 func _ready() -> void:
-	show_dialogue()
-
-func _process(delta: float) -> void:
-	pass
+	await get_tree().process_frame
+	DialogueManager.show_dialogue_balloon(
+		load("res://dialogues/dialogue01.dialogue"),
+        "start"
+	)
