b3579d18a588e46ad54179759cb1461e78fed91b
Gian Piero Cano <gianpierodaniel@gmail.com>
2026-04-29 17:13:51 -0500

    dialogos
    di

--- dialogue.gd
+++ dialogue.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+var balloon_scene = preload("res://dialogues/balloon.tscn")
 
-# Called when the node enters the scene tree for the first time.
+func show_dialogue():
+	var balloon = balloon_scene.instantiate()
+	get_tree().current_scene.add_child(balloon)
+
 func _ready() -> void:
-	pass # Replace with function body.
+	show_dialogue()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
