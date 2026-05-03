9611abcb6c214d7946765b15c5fa78f789348fb1
Leopoldo Brito Ruiz <britoleopoldo02@gmail.com>
2026-05-03 11:52:58 -0500

    Cambio en la voz y arreglo de escena

--- scripts/levels/lvl_1_reception_apagado.gd
+++ scripts/levels/lvl_1_reception_apagado.gd
@@ -1,12 +1,11 @@
 # lvl1_reception_apagado.gd
 extends Node2D
 
-@onready var btn_volver = $BtnVolver
+@onready var btn_volver = $CanvasLayer/BtnVolver
 
 func _ready():
 	btn_volver.pressed.connect(_on_volver)
 	
-	# Conectar cursor solo si no está ya conectado
 	if not btn_volver.mouse_entered.is_connected(CursorManager.set_hover):
 		btn_volver.mouse_entered.connect(CursorManager.set_hover)
 	if not btn_volver.mouse_exited.is_connected(CursorManager.set_normal):
--- scripts/ui/dialogue/balloon.gd
+++ scripts/ui/dialogue/balloon.gd
@@ -145,6 +145,8 @@ func apply_dialogue_line() -> void:
 
 	dialogue_label.show()
 	if not dialogue_line.text.is_empty():
+		if dialogue_line.character == "NX-0":
+			VoiceManager.play_voz()
 		dialogue_label.type_out()
 		await dialogue_label.finished_typing
 
