820ff474529b60894884ee30bae30999f867fb9e
Leopoldo Brito Ruiz <britoleopoldo02@gmail.com>
2026-05-05 00:20:44 -0500

    Efectos de paso

--- scripts/ui/effects/TransitionManager.gd
+++ scripts/ui/effects/TransitionManager.gd
@@ -3,14 +3,32 @@ extends CanvasLayer
 
 @onready var overlay = $Overlay
 
+var sfx_pasos: Array = []
+var player_pasos: AudioStreamPlayer
+
 func _ready():
 	overlay.color = Color(0, 0, 0, 0)
 	overlay.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+	# Cargar sonidos de caminata
+	player_pasos = AudioStreamPlayer.new()
+	add_child(player_pasos)
+	sfx_pasos = [
+		load("res://assets/sounds/Econexo_Movimiento1.wav"),
+		load("res://assets/sounds/Econexo_Movimiento2.wav"),
+		load("res://assets/sounds/Econexo_Movimiento3.wav")
+	]
 
 func fade_to(scene_path: String, duration: float = 0.5):
 	overlay.mouse_filter = Control.MOUSE_FILTER_STOP
 	TooltipUi.hide_tooltip()
 	
+	# Reproducir sonido de caminata aleatorio
+	if not sfx_pasos.is_empty():
+		player_pasos.stream = sfx_pasos[randi() % sfx_pasos.size()]
+		player_pasos.play()
+	
+	# Fade a negro
 	var tween = create_tween()
 	tween.tween_property(overlay, "color:a", 1.0, duration)
 	await tween.finished
@@ -20,6 +38,7 @@ func fade_to(scene_path: String, duration: float = 0.5):
 	await get_tree().process_frame
 	await get_tree().process_frame
 	
+	# Fade desde negro
 	var tween2 = create_tween()
 	tween2.tween_property(overlay, "color:a", 0.0, duration)
 	await tween2.finished
